94a6eb469ba4fdd1bbe429d1fbe676723c725f2a
shamil <shamil880699@gmail.com>
2026-03-24 15:54:39 +0530

    added loginUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,12 @@ import Community from './pages/Community'
 import { assets } from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
+import { useAppContext } from './context/AppContext'
+import Login from './pages/Login'
 
 const App = () => {
+
+  const {user} = useAppContext()
   const [isMenuOpen,setIsMenuOpen]=useState(false)
   const{pathname}=useLocation()
   if(pathname==='/loading')return <Loading/>
@@ -16,6 +20,7 @@ const App = () => {
     <>
       {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=>setIsMenuOpen(true)}/>}
       <div className='dark:bg-linear-to-b from-[#242121] to-[#000000] dark:text-white'>
+        {user ? (
         <div className='flex h-screen w-screen'>
           <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
           <Routes>
@@ -24,6 +29,12 @@ const App = () => {
             <Route path='/community' element={<Community/>}/>
           </Routes>
         </div>
+        ) : (
+          <div className='bg-linear-to-b  from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+            <Login/>
+          </div>
+        )}
+        
       </div>
     </>
   )
